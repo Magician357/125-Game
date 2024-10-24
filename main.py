3324da6e296657eea922ab8416361b619a4e0f88
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-10-24 15:37:42 -0700

    freeze and other changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from sand import *
+# NOTE: You must install this file (using pip)
+from bresenham import bresenham
+# All this is used for is to draw lines
 
 # pygame setup
 pygame.init()
@@ -18,15 +21,27 @@ fire_button = pygame.rect.Rect(230,710,100,30)
 fire_text = font.render("Fire",True,(0,0,0))
 oil_button = pygame.rect.Rect(340,710,100,30)
 oil_text = font.render("Oil",True,(0,0,0))
+freeze_button = pygame.rect.Rect(450,710,100,30)
+freeze_text = font.render("Freeze",True,(0,0,0))
+wood_button = pygame.rect.Rect(560,710,100,30)
+wood_text = font.render("Wood",True,(0,0,0))    
 
 fps = 30
 
 mouse_down = False
 
-draw_type=liquid
+draw_type=wood
+
+draw_size=0
+
+simulating = True
+
+i_x,i_y=0,0
+p_x,p_y=0,0
 
 while running:
     mouse_pos = pygame.mouse.get_pos()
+    p_x,p_y=i_x,i_y
     i_x,i_y=universe.get_index(mouse_pos)
     
     # check for events
@@ -43,18 +58,30 @@ while running:
                 draw_type=fire
             elif oil_button.collidepoint(mouse_pos):
                 draw_type=oil
+            elif freeze_button.collidepoint(mouse_pos):
+                draw_type=freeze
+            elif wood_button.collidepoint(mouse_pos):
+                draw_type=wood
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_down=False
+        elif event.type == pygame.KEYDOWN:
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_EQUALS]:
+                draw_size+=1
+            elif pressed[pygame.K_MINUS]:
+                draw_size = min(0,draw_size)
+            if pressed[pygame.K_SPACE]:
+                simulating=not simulating
     
     if mouse_down:
         if i_x != -1 and universe.grid[i_y][i_x].density == -1:
-            universe.set_cell(i_x,i_y,draw_type(i_x,i_y))
+            # universe.set_cell(i_x,i_y,draw_type(i_x,i_y))
+            for c_x,c_y in bresenham(p_x if p_x != -1 else i_x,p_y if p_y != -1 else i_y,i_x,i_y):
+                universe.set_box(c_x,c_y,draw_size,draw_type,False)
 
 
     # clear frame
     screen.fill("white")
-    
-    universe.draw(screen)
     
     pygame.draw.rect(screen,(203,189,147),powder_button)
     screen.blit(powder_text,(15,715))
@@ -64,11 +91,19 @@ while running:
     screen.blit(fire_text,(235,715))
     pygame.draw.rect(screen,(64, 31, 0),oil_button)
     screen.blit(oil_text,(345,715))
+    pygame.draw.rect(screen,(25, 100, 227),freeze_button)
+    screen.blit(freeze_text,(455,715))
+    pygame.draw.rect(screen,(71, 61, 31),wood_button)
+    screen.blit(wood_text,(565,715))
     
-    universe.tick()
+    if simulating:
+        universe.tick()
+    universe.draw(screen)
+    
+    pygame.draw.rect(screen,(50,50,50),universe.rects[i_y][i_x])
 
 
     pygame.display.flip()
-    pygame.display.set_caption(f"fps: {clock.get_fps():.2f}     cell info: ({i_x},{i_y}), {universe.grid[i_y][i_x].name}, {universe.grid[i_y][i_x].temp:.2f} degrees")
+    pygame.display.set_caption(f"fps: {clock.get_fps():.2f}     cell info: ({i_x},{i_y}), {universe.grid[i_y][i_x].name}, {universe.grid[i_y][i_x].temp:.2f} degrees, {'Active' if (i_x,i_y) in universe.active else 'Inactive'}")
 
     clock.tick(fps)
--- sand.py
+++ sand.py
@@ -74,19 +74,20 @@ class universe_class:
         for x,y in copy(self.active):
             if self.grid[y][x].clock < self.cycle:
                 
-                if self.grid[y][x].density == -1 and abs(self.grid[y][x].temp-21) < 1.5:
+                self.grid[y][x].tick_temp()
+                
+                if self.grid[y][x].density == -1 and abs(self.grid[y][x].temp-21) < 0.5:
                     self.active.remove((x,y))
                     continue
                 
                 self.grid[y][x].tick()
-                self.grid[y][x].tick_temp()
                 # dx, dy, check value, max value
                 for dx,dy,cv,mv in zip((-1,1,0,0),(0,0,-1,1),(x,x,y,y),(0,self.width-1,0,self.height-1)):
                     if cv != mv:
                         # if self.grid[y+dy][x+dx].density == -1:
                         #     self.grid[y+dy][x+dx].temp+= 0.01*(21-self.grid[y+dy][x+dx].temp)
                         t1,t2 = self.grid[y+dy][x+dx].temp,self.grid[y][x].temp
-                        t1,t2 = self.balance_heat(t1,t2,0.01 if self.grid[y+dy][x+dx].density == -1 else 0.4)
+                        t1,t2 = self.balance_heat(t1,t2) #0.01 if self.grid[y+dy][x+dx].density == -1 else 0.4
                         self.grid[y+dy][x+dx].temp,self.grid[y][x].temp = t1,t2
         self.cycle+=1
     
@@ -104,8 +105,17 @@ class universe_class:
         return -1,-1
 
     def set_cell(self,x,y,value):
-        self.grid[y][x] = value
-        self.active.add((x,y))
+        if x >= 0 and x < self.width and y >= 0 and y < self.height:
+            self.grid[y][x] = value
+            self.active.add((x,y))
+    
+    def set_box(self,cx,cy,s,cell_type,override=True):
+        for dx in range(-s,s+1):
+            for dy in range(-s,s+1):
+                if cx+dx >= self.width or cx+dx < 0 or cy+dy >= self.height or cy+dy < 0:
+                    continue
+                elif (universe.grid[cy+dy][cx+dx].density == -1 or override):
+                    self.set_cell(cx+dx,cy+dy,cell_type(cx+dx,cy+dy))
 
     def balance_heat(self,temp1,temp2,diffusion_rate=0.15):
         temp_diff = temp2-temp1
@@ -113,7 +123,7 @@ class universe_class:
         return temp1+temp_diff,temp2-temp_diff
 
 # The variable used by every cell
-universe = universe_class(70,70,700,700)
+universe = universe_class(40,40,700,700)
 
 class powder(empty):
     def __init__(self,x,y,density=10,color=(203,189,147)):
@@ -175,6 +185,9 @@ class liquid(empty):
         if self.temp >= 100:
             universe.grid[self.y][self.x] = gas(self.x,self.y)
             universe.grid[self.y][self.x].temp=max(self.temp,150)
+        elif self.temp <= 0:
+            universe.grid[self.y][self.x] = ice(self.x,self.y)
+            universe.grid[self.y][self.x].temp=min(-10,self.temp)
         else:
             if self.burning:
                 self.burning=False
@@ -188,16 +201,17 @@ class fire(empty):
         self.density=0
         self.x, self.y = x,y
         self.clock = 0
-        self.name = "liquid"
+        self.name = "fire"
         self.temp=500
         self.ticks    = 0              # How many ticks it has been alive
-        self.lifespan = randint(5,35)  # How long it should last
+        self.lifespan = randint(5,15)  # How long it should last
     def tick(self):
         self.burn_neighbors()
         
         self.ticks+=1
         if self.ticks >= self.lifespan:
             universe.grid[self.y][self.x]=empty(self.x,self.y)
+            return
         
         dx=rand_dir()
         dy=-1 if self.y > 0 else 0
@@ -228,7 +242,7 @@ class gas(empty):
         self.density=density
         self.x, self.y = x,y
         self.clock = 0
-        self.name = "liquid"
+        self.name = "gas"
         self.temp=150
         self.burning=False
         
@@ -238,16 +252,36 @@ class gas(empty):
             dx=0
         if self.y == (0,universe.height-1,0)[dy]:
             dy=0
-        self.swap(dx,dy)
+        if universe.grid[self.y+dy][self.x+dx].density == -1:
+                    self.swap(dx,dy)
     def tick_temp(self):
         if self.temp < 80:
             universe.grid[self.y][self.x] = liquid(self.x,self.y)
             universe.grid[self.y][self.x].temp=self.temp
+            return
         if self.burning:
             self.burning=False
-            self.temp += 0.1 * (500-self.temp)
+            self.temp += 0.01 * (21-self.temp)
 
-# Subclasses
+class ice(empty):
+    def __init__(self,x,y,density=100,color=(196, 219, 255)):
+        self.x,self.y=x,y
+        self.clock=0
+        self.temp=-10
+        self.color=color
+        self.density=density
+        self.name="ice"
+    def tick(self):
+        pass
+        # ice doesn't move
+    def tick_temp(self):
+        if self.temp > 0:
+            replacement=liquid(self.x,self.y)
+            replacement.temp=self.temp
+            universe.grid[self.y][self.x] = replacement
+        else:
+            self.temp += 0.15 * (-100-self.temp)
+
 class oil(liquid):
     def __init__(self, x, y, density=4, color=(64, 31, 0)):
         super().__init__(x, y, density, color)
@@ -269,3 +303,52 @@ class oil(liquid):
     def tick_temp(self):
         if self.burning:
             self.temp+= 0.5 * (500-self.temp)
+
+class wood(empty):
+    def __init__(self,x,y,density=100,color=(71, 61, 31)):
+        self.x,self.y=x,y
+        self.clock=0
+        self.density=density
+        self.color=color
+        self.burning=False
+        self.burn_ticks = 0
+        self.burn_max = randint(20,50)
+        self.temp=21
+        self.name="wood"
+    def tick(self):
+        # wood does not move
+        if self.burning:
+            self.burn_ticks+=1
+            if self.y > 0 and universe.grid[self.y-1][self.x].density == -1 and randint(1,5) == 2:
+                universe.set_cell(self.x,self.y-1,fire(self.x,self.y-1))
+            if self.burn_ticks >= self.burn_max:
+                universe.grid[self.y][self.x]=fire(self.x,self.y)
+                return
+            self.burn_neighbors()
+    def tick_temp(self):
+        if self.burning:
+            self.temp+= 0.5 * (500-self.temp)
+
+class freeze(empty):
+    # instantly turns into a cold empty cell
+    def __init__(self,x,y):
+        self.x,self.y=x,y
+        self.temp=-50
+        self.name="freeze"
+        self.clock=0
+        self.density=0
+        self.ticks=0
+        self.max_ticks=randint(1,5)
+    
+    @property
+    def color(self):
+        return (randint(0, 50), randint(50, 150),randint(200, 255))
+
+    def tick(self):
+        self.ticks+=1
+        if self.ticks >= self.max_ticks:
+            replacement = empty(self.x,self.y)
+            replacement.temp=self.temp
+            universe.grid[self.y][self.x]=replacement
+    def tick_temp(self):
+        self.temp += 0.5 * (-50 - self.temp)
