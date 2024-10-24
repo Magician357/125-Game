81f5c325a91fc95fefb5f03a75dbab0eb319a305
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-10-24 12:45:19 -0700

    oil

--- main.py
+++ main.py
@@ -23,33 +23,32 @@ fps = 30
 
 mouse_down = False
 
-draw_type=powder
+draw_type=liquid
 
 while running:
+    mouse_pos = pygame.mouse.get_pos()
+    i_x,i_y=universe.get_index(mouse_pos)
+    
     # check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_down=True
-            
-            pos = pygame.mouse.get_pos()
-            if powder_button.collidepoint(pos):
+            if powder_button.collidepoint(mouse_pos):
                 draw_type=powder
-            elif water_button.collidepoint(pos):
+            elif water_button.collidepoint(mouse_pos):
                 draw_type=liquid
-            elif fire_button.collidepoint(pos):
+            elif fire_button.collidepoint(mouse_pos):
                 draw_type=fire
-            elif oil_button.collidepoint(pos):
+            elif oil_button.collidepoint(mouse_pos):
                 draw_type=oil
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_down=False
     
     if mouse_down:
-        mouse_pos = pygame.mouse.get_pos()
-        i_x,i_y=universe.get_index(mouse_pos)
-        if i_x != -1:
-            universe.grid[i_y][i_x] = draw_type(i_x,i_y)
+        if i_x != -1 and universe.grid[i_y][i_x].density == -1:
+            universe.set_cell(i_x,i_y,draw_type(i_x,i_y))
 
 
     # clear frame
@@ -70,6 +69,6 @@ while running:
 
 
     pygame.display.flip()
-    pygame.display.set_caption(f"fps: {clock.get_fps():.2f}")
+    pygame.display.set_caption(f"fps: {clock.get_fps():.2f}     cell info: ({i_x},{i_y}), {universe.grid[i_y][i_x].name}, {universe.grid[i_y][i_x].temp:.2f} degrees")
 
     clock.tick(fps)
--- sand.py
+++ sand.py
@@ -1,7 +1,7 @@
 # This file is not meant to be run.
 # This file has all of the code to run the game.
 import pygame
-from copy import deepcopy
+from copy import deepcopy,copy
 from random import randint
 
 def rand_dir():
@@ -19,22 +19,29 @@ class empty:
         self.burning=False
         self.burn_ticks = 0
         self.burn_max = 10
+        self.heat_transfer=0
     def tick(self):
-        pass
+        if self.y > 0 \
+            and universe.grid[self.y-1][self.x].density == -1\
+                and universe.grid[self.y-1][self.x].temp < self.temp:
+                    self.swap(0,-1)
     def tick_temp(self):
         #cool
-        self.temp += 0.5 * (21-self.temp)
+        self.temp += 0.01 * -1 if self.temp > 21 else 1
+        # pass
     def swap(self,dx,dy):
         global universe
         
         self.clock = universe.cycle
-        temp=deepcopy(universe[self.y+dy][self.x+dx]) # store before swapping
+        temp=deepcopy(universe.grid[self.y+dy][self.x+dx]) # store before swapping
         temp.y,temp.x=self.y,self.x
         copy = deepcopy(self)
         copy.y+=dy
         copy.x+=dx
         universe.grid[self.y+dy][self.x+dx] = copy
         universe.grid[self.y][self.x] = temp
+        
+        universe.active.add((self.x+dx,self.y+dy))
         
     def burn_neighbors(self):
         global universe
@@ -59,20 +66,28 @@ class universe_class:
         
         self.rects = [[pygame.Rect(x * cell_width, y * cell_height, cell_width, cell_height) for x in range(width)] for y in range(height)]
         
+        self.active=set()
+        
     def tick(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.grid[y][x].clock < self.cycle:
-                    self.grid[y][x].tick()
-                    self.grid[y][x].tick_temp()
-                    if x > 0:
-                        t1,t2 = self.grid[y][x-1].temp,self.grid[y][x].temp
-                        t1,t2 = self.balance_heat(t1,t2)
-                        self.grid[y][x-1].temp,self.grid[y][x].temp = t1,t2
-                    if y > 0:
-                        t1,t2 = self.grid[y-1][x].temp,self.grid[y][x].temp
-                        t1,t2 = self.balance_heat(t1,t2)
-                        self.grid[y-1][x].temp,self.grid[y][x].temp = t1,t2
+        # for y in range(self.height):
+        #     for x in range(self.width):
+        for x,y in copy(self.active):
+            if self.grid[y][x].clock < self.cycle:
+                
+                if self.grid[y][x].density == -1 and abs(self.grid[y][x].temp-21) < 1.5:
+                    self.active.remove((x,y))
+                    continue
+                
+                self.grid[y][x].tick()
+                self.grid[y][x].tick_temp()
+                # dx, dy, check value, max value
+                for dx,dy,cv,mv in zip((-1,1,0,0),(0,0,-1,1),(x,x,y,y),(0,self.width-1,0,self.height-1)):
+                    if cv != mv:
+                        # if self.grid[y+dy][x+dx].density == -1:
+                        #     self.grid[y+dy][x+dx].temp+= 0.01*(21-self.grid[y+dy][x+dx].temp)
+                        t1,t2 = self.grid[y+dy][x+dx].temp,self.grid[y][x].temp
+                        t1,t2 = self.balance_heat(t1,t2,0.01 if self.grid[y+dy][x+dx].density == -1 else 0.4)
+                        self.grid[y+dy][x+dx].temp,self.grid[y][x].temp = t1,t2
         self.cycle+=1
     
     def draw(self,screen):
@@ -87,11 +102,12 @@ class universe_class:
                 if self.rects[y][x].collidepoint(pos):
                     return x,y
         return -1,-1
-    
-    def __getitem__(self,y):
-        return self.grid[y]
 
-    def balance_heat(self,temp1,temp2,diffusion_rate=0.4):
+    def set_cell(self,x,y,value):
+        self.grid[y][x] = value
+        self.active.add((x,y))
+
+    def balance_heat(self,temp1,temp2,diffusion_rate=0.15):
         temp_diff = temp2-temp1
         temp_diff*=diffusion_rate
         return temp1+temp_diff,temp2-temp_diff
@@ -140,6 +156,7 @@ class liquid(empty):
         self.clock = 0
         self.name = "liquid"
         self.temp=21
+        self.burning=False
     def tick(self):
         dir = rand_dir()
         
@@ -156,9 +173,14 @@ class liquid(empty):
             return
     def tick_temp(self):
         if self.temp >= 100:
-            universe.grid[self.y][self.x] = empty(self.x,self.y)
+            universe.grid[self.y][self.x] = gas(self.x,self.y)
+            universe.grid[self.y][self.x].temp=max(self.temp,150)
         else:
-            self.temp = 0.05 * (21 - self.temp)
+            if self.burning:
+                self.burning=False
+                self.temp += 0.1 * (110 - self.temp)
+            else:
+                self.temp += 0.05 * (21 - self.temp)
 
 class fire(empty):
     def __init__(self,x,y):
@@ -183,6 +205,8 @@ class fire(empty):
         if self.x == (0,universe.width-1,0)[dx]:
             dx=0
         
+        universe.grid[self.y+dy][self.x+dx].temp += 0.2 * (self.temp-universe.grid[self.y+dy][self.x+dx].temp)
+        
         if universe.grid[self.y+dy][self.x+dx].density == -1:
             self.swap(dx,dy)
         elif universe.grid[self.y+dy][self.x].density == -1:
@@ -190,14 +214,38 @@ class fire(empty):
         elif universe.grid[self.y][self.x+dx].density == -1:
             self.swap(dx,0)
     def tick_temp(self):
-        self.temp = 0.1 * (1000-self.temp)
+        self.temp += 0.1 * (1000-self.temp)
     
     # random color every time
     @property
     def color(self):
         return (randint(200, 255), randint(50, 150), randint(0, 50))
 
-
+class gas(empty):
+    def __init__(self,x,y,density=1,color=(232//2, 243//2, 255//2)):
+        # For what these values are, see empty class
+        self.color=color
+        self.density=density
+        self.x, self.y = x,y
+        self.clock = 0
+        self.name = "liquid"
+        self.temp=150
+        self.burning=False
+        
+    def tick(self):
+        dx,dy = rand_dir(),(-1,-1,1)[randint(0,2)]
+        if self.x == (0,universe.width-1,0)[dx]:
+            dx=0
+        if self.y == (0,universe.height-1,0)[dy]:
+            dy=0
+        self.swap(dx,dy)
+    def tick_temp(self):
+        if self.temp < 80:
+            universe.grid[self.y][self.x] = liquid(self.x,self.y)
+            universe.grid[self.y][self.x].temp=self.temp
+        if self.burning:
+            self.burning=False
+            self.temp += 0.1 * (500-self.temp)
 
 # Subclasses
 class oil(liquid):
@@ -211,7 +259,7 @@ class oil(liquid):
         if self.burning:
             self.burn_ticks+=1
             if self.y > 0 and universe.grid[self.y-1][self.x].density == -1 and randint(1,5) == 2:
-                universe.grid[self.y-1][self.x] = fire(self.x,self.y-1)
+                universe.set_cell(self.x,self.y-1,fire(self.x,self.y-1))
             if self.burn_ticks >= self.burn_max:
                 universe.grid[self.y][self.x]=fire(self.x,self.y)
                 return
